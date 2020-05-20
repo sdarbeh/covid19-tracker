cf23916806b47d5816297c53bb16d5499ff30524
sdarbeh <56693052+sdarbeh@users.noreply.github.com>
2020-05-20 18:35:52 -0500

    Updates

---
+++ precache-manifest.6284b7d37ba33a797a220121e2b8311a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e825300778c1fb4b87c7f0473d0d48",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "50c76adf8756a5e81351",
+    "url": "/covid19-tracker/static/css/main.396e13c9.chunk.css"
+  },
+  {
+    "revision": "883ce4246fc40d7229db",
+    "url": "/covid19-tracker/static/js/2.a50918f7.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19-tracker/static/js/2.a50918f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c76adf8756a5e81351",
+    "url": "/covid19-tracker/static/js/main.9964f0e6.chunk.js"
+  },
+  {
+    "revision": "8cd54aa8f9c07f9dab41",
+    "url": "/covid19-tracker/static/js/runtime-main.c8a8ed36.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
